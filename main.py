8f1f5e01c04222e7f342f1198d31a5ac710525b8
Shkoobyari <69491479+Shkoobyari@users.noreply.github.com>
2020-09-12 13:34:28 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,43 +1,48 @@
-import os
 import csv
+import os
+csv_path = os.path.join('.', 'Resources', 'budget_data.csv')
+final_txt_file_path = os.path.join('.', 'Analysis', 'pybank.txt')
 
-csv_file_path = os.path.join('./', 'Resources', 'election_data.csv')
-final_txt_file_path = os.path.join('./', 'Analysis', 'pypoll.txt')
+n_months = 0
+net_profit = 0
+greatest_increase_amount = 0
+greatest_increase_month_year = ''
+greatest_decrease_amount = 0
+greatest_decrease_month_year = ''
+average_change = 0
 
-total_votes = 0
-candidates = []
-vote_talley = {}
-
-with open(csv_file_path) as csvfile:
+with open(csv_path) as csvfile:
   csv_reader = csv.reader(csvfile, delimiter=',')
 
   header = next(csv_reader)
 
+  first_row = next(csv_reader)
+
+  previous_month_net = int(first_row[1])
+  monthly_change = 0
+
   for row in csv_reader:
-    total_votes += 1
-    candidate = row[-1]
-    if candidate not in candidates:
-      candidates.append(candidate)
-      vote_talley[candidate] = 1
-    else:
-      vote_talley[candidate] += 1
-print(vote_talley)
-# Analyze data
+    current_month_net = int(row[1])
 
-final_vote = {}
-for key, val in vote_talley.items():
-  final_vote[key] = {
-    'vote_number': val,
-    'vote_percentage': (val / total_votes) * 100
-  }
-winner = ''
-dummy_counter = 0
-for key, val in final_vote.items():
-  if val['vote_number'] > dummy_counter:
-    dummy_counter = val['vote_number']
-    winner = key
+    n_months += 1
+    net_profit += current_month_net
 
-print(f"""Election Results \n-------------------- \nTotal Votes: {total_votes} \n-------------------- \nKhan: {final_vote['Khan']['vote_percentage']} ({final_vote['Khan']['vote_number']})\nCorrey: {final_vote['Correy']['vote_percentage']} ({final_vote['Correy']['vote_number']}) \nLi: {final_vote['Li']['vote_percentage']} ({final_vote['Li']['vote_number']}) \nO'Tooley: {final_vote["O'Tooley"]['vote_percentage']} ({final_vote["O'Tooley"]['vote_number']}) \n -------------------- \nWinner: {winner} \n--------------------""")
+    # Conditional statements for dealing with negative number
+    if current_month_net > 0:
+      monthly_change = previous_month_net - current_month_net
+    elif current_month_net < 0:
+      monthly_change = previous_month_net + current_month_net
+      
+    if monthly_change > 0:
+      if monthly_change > greatest_increase_amount:
+        greatest_increase_amount = monthly_change
+        greatest_increase_month_year = row[0]
+    elif monthly_change < 0:
+      if monthly_change < greatest_decrease_amount:
+        greatest_decrease_amount = monthly_change
+        greatest_decrease_month_year = row[0]
+  average_change = net_profit / n_months
+  print(f"Financial Analysis \n-------------------------- \nTotal Months: {n_months} \nTotal: ${net_profit}\nAverage Change: ${average_change} \nGreatest Increase in Profits: {greatest_increase_month_year} (${greatest_increase_amount}) \nGreatest Decrease in Profits: {greatest_decrease_month_year} (${greatest_decrease_amount})")
 
 with open(final_txt_file_path, "w") as txtfile:
-  txtfile.write(f"""Election Results \n-------------------- \nTotal Votes: {total_votes} \n-------------------- \nKhan: {final_vote['Khan']['vote_percentage']} ({final_vote['Khan']['vote_number']})\nCorrey: {final_vote['Correy']['vote_percentage']} ({final_vote['Correy']['vote_number']}) \nLi: {final_vote['Li']['vote_percentage']} ({final_vote['Li']['vote_number']}) \nO'Tooley: {final_vote["O'Tooley"]['vote_percentage']} ({final_vote["O'Tooley"]['vote_number']}) \n -------------------- \nWinner: {winner} \n--------------------""")
+  txtfile.write(f"Financial Analysis \n-------------------------- \nTotal Months: {n_months} \nTotal: ${net_profit}\nAverage Change: ${average_change} \nGreatest Increase in Profits: {greatest_increase_month_year} (${greatest_increase_amount}) \nGreatest Decrease in Profits: {greatest_decrease_month_year} (${greatest_decrease_amount})")
